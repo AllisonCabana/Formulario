127c3a6fedc0b7330d0f1281be13a24a0239eb84
Allison Cabana <allisonmcabanag@gmail.com>
2022-05-11 16:07:26 -0300

    Calculadora y Formulario

---
+++ Formulario/Formulario/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botGuardar = new System.Windows.Forms.Button();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtFecha = new System.Windows.Forms.TextBox();
+            this.txtSapellido = new System.Windows.Forms.TextBox();
+            this.txtPapellido = new System.Windows.Forms.TextBox();
+            this.txtSnombre = new System.Windows.Forms.TextBox();
+            this.txtPnombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // botGuardar
+            // 
+            this.botGuardar.Location = new System.Drawing.Point(121, 292);
+            this.botGuardar.Name = "botGuardar";
+            this.botGuardar.Size = new System.Drawing.Size(102, 32);
+            this.botGuardar.TabIndex = 0;
+            this.botGuardar.Text = "Guardar";
+            this.botGuardar.UseVisualStyleBackColor = true;
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(43, 23);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(67, 20);
+            this.txtCedula.TabIndex = 1;
+            // 
+            // txtFecha
+            // 
+            this.txtFecha.Location = new System.Drawing.Point(43, 153);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(67, 20);
+            this.txtFecha.TabIndex = 2;
+            // 
+            // txtSapellido
+            // 
+            this.txtSapellido.Location = new System.Drawing.Point(43, 127);
+            this.txtSapellido.Name = "txtSapellido";
+            this.txtSapellido.Size = new System.Drawing.Size(67, 20);
+            this.txtSapellido.TabIndex = 3;
+            // 
+            // txtPapellido
+            // 
+            this.txtPapellido.Location = new System.Drawing.Point(43, 101);
+            this.txtPapellido.Name = "txtPapellido";
+            this.txtPapellido.Size = new System.Drawing.Size(67, 20);
+            this.txtPapellido.TabIndex = 4;
+            // 
+            // txtSnombre
+            // 
+            this.txtSnombre.Location = new System.Drawing.Point(43, 75);
+            this.txtSnombre.Name = "txtSnombre";
+            this.txtSnombre.Size = new System.Drawing.Size(67, 20);
+            this.txtSnombre.TabIndex = 5;
+            // 
+            // txtPnombre
+            // 
+            this.txtPnombre.Location = new System.Drawing.Point(43, 49);
+            this.txtPnombre.Name = "txtPnombre";
+            this.txtPnombre.Size = new System.Drawing.Size(67, 20);
+            this.txtPnombre.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Cedula";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Fecha de Nacimiento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Primer Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(157, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Segundo Apellido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(157, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Segundo Nombre";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(157, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Primer Nombre";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(157, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Telefono";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(157, 184);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Direccion";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(157, 238);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Email";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(43, 181);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(67, 20);
+            this.txtDireccion.TabIndex = 16;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(43, 208);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(67, 20);
+            this.txtTel.TabIndex = 17;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(43, 235);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(67, 20);
+            this.txtEmail.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 349);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPnombre);
+            this.Controls.Add(this.txtSnombre);
+            this.Controls.Add(this.txtPapellido);
+            this.Controls.Add(this.txtSapellido);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.botGuardar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botGuardar;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtFecha;
+        private System.Windows.Forms.TextBox txtSapellido;
+        private System.Windows.Forms.TextBox txtPapellido;
+        private System.Windows.Forms.TextBox txtSnombre;
+        private System.Windows.Forms.TextBox txtPnombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtEmail;
+    }
+}
+
---
+++ repositorio4/repositorio/repositorio/Calculadora.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace repositorio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Valor1 =Convert.ToInt32(txtValor1.Text);
+            int Valor2 = Convert.ToInt32(txtValor2.Text);
+            int Total = Valor1 + Valor2;
+            txtTotal.Text = Total.ToString();
+        //    MessageBox.Show(Total.ToString());
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Valor1 = Convert.ToInt32(txtValor1.Text);
+            int Valor2 = Convert.ToInt32(txtValor2.Text);
+            int Total = Valor2 -Valor1;
+            txtTotal.Text = Total.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int Valor1 = Convert.ToInt32(txtValor1.Text);
+            int Valor2 = Convert.ToInt32(txtValor2.Text);
+            int Total = Valor1 * Valor2;
+            txtTotal.Text = Total.ToString();
+        }
+
+        private void Resto_Click(object sender, EventArgs e)
+        {
+            decimal Valor1 = Convert.ToDecimal(txtValor1.Text);
+            decimal Valor2 = Convert.ToDecimal (txtValor2.Text);
+            decimal Total = 0;
+            if (Valor1 != 0)
+            {
+                Total = Valor2 / Valor1;
+            }
+            else { MessageBox.Show("No se puede dividir entre 0"); }
+
+            txtTotal.Text = Total.ToString();
+        }
+
+        
+       
+    }
+}
